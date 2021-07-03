5db9da16fd90ced900e1a94d9a90011242433376
warunaudayanga <warunaudayangalakruwan@gmail.com>
2021-07-03 18:56:46 +0530

    0.1

--- js/index.js
+++ js/index.js
@@ -1,11 +1,37 @@
+// noinspection JSJQueryEfficiency
+
+let lastPage;
+
 $(() => {
-    $('.banner').ripples();
-    $(document).scroll(() => {
-        if ($(this)[0].scrollY >= $('.banner').height() - 70) $('.navbar-container').addClass('scrolled');
-        else $('.navbar-container').removeClass('scrolled');
-    });
+    load('home');
 });
 
 $(document).on('click', '.click-me', () => {
     $('html').animate({scrollTop: window.innerHeight - 70}, 0);
 });
+
+$(document).on('click', '.nav-link', (e) => {
+    e.preventDefault();
+    load($(e.target).data('page'), $(e.target).data('id'));
+});
+
+const load = (page, id) => {
+    if(page !== 'home') {
+        $('.banner-container').addClass('hide');
+        $('body').removeClass('padding');
+    } else {
+        $('.banner-container').removeClass('hide');
+        $('body').addClass('padding');
+    }
+    if(lastPage !== page) {
+        $('#content').html('<div class="spinner-box"><div class="spinner-border" role="status"></div></div>');
+        $.get(page + '.html', (data) => {
+            $('#content').html(data);
+            $('html, body').scrollTop($(id? '#' + id: 'body').offset().top);
+            lastPage = page;
+        });
+    } else {
+        $('html, body').animate({scrollTop: $(id? '#' + id: 'body').offset().top}, 0);
+    }
+
+}
